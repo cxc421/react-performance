98bfd135b0d1e018d3aea9f8c8b8f36429b37cd2
Chris Chuang <chrischuang0421@gmail.com>
2020-11-03 13:38:57 +0800

    Exercise 1 - Add prefetch webpack magic comment

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -1,12 +1,11 @@
 // Code splitting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
-const loadGlobe = () => import('../globe')
-const Globe = React.lazy(loadGlobe)
+import * as React from 'react';
+const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'));
 
 function App() {
-  const [showGlobe, setShowGlobe] = React.useState(false)
+  const [showGlobe, setShowGlobe] = React.useState(false);
 
   return (
     <div
@@ -19,11 +18,7 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label
-        style={{marginBottom: '1rem'}}
-        onFocus={loadGlobe}
-        onMouseOver={loadGlobe}
-      >
+      <label style={{marginBottom: '1rem'}}>
         <input
           type="checkbox"
           checked={showGlobe}
@@ -37,10 +32,10 @@ function App() {
         </React.Suspense>
       </div>
     </div>
-  )
+  );
 }
 // 🦉 Note that if you're not on the isolated page, then you'll notice that this
 // app actually already has a React.Suspense component higher up in the tree
 // where this component is rendered, so you *could* just rely on that one.
 
-export default App
+export default App;
