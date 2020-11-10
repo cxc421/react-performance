8e7def9eb7ea8c61d58056f4c3d3c47ad36e847c
Chris Chuang <chrischuang0421@gmail.com>
2020-11-10 10:50:20 +0800

    Exercise 7 - Add production performance monitoring

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -1,15 +1,16 @@
 // Production performance monitoring
 // http://localhost:3000/isolated/exercise/07.js
 
-import * as React from 'react'
+import * as React from 'react';
+import {unstable_trace as trace} from 'scheduler/tracing';
 // 🐨 you're going to need the reportProfile function
-// 💰 here, let me help you with that...
-// import reportProfile from '../report-profile'
+import reportProfile from '../report-profile';
 
 function Counter() {
-  const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>{count}</button>
+  const [count, setCount] = React.useState(0);
+  const increment = () =>
+    trace('Counter click', performance.now(), () => setCount(c => c + 1));
+  return <button onClick={increment}>{count}</button>;
 }
 
 function App() {
@@ -20,16 +21,18 @@ function App() {
       give it the ID of "counter" and pass reportProfile
       to the onRender prop.
       */}
-      <div>
-        Profiled counter
-        <Counter />
-      </div>
+      <React.Profiler id="counter" onRender={reportProfile}>
+        <div>
+          Profiled counter
+          <Counter />
+        </div>
+      </React.Profiler>
       <div>
         Unprofiled counter
         <Counter />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
